0e7bcf3953b331ed2c5a6f462e2d01087003a30a
purr100 <purr@moderato.app>
2024-11-11 02:55:38 +0800

    add usage to chunk

--- Sources/OpenAI/Public/Parameters/Chat/ChatCompletionParameters.swift
+++ Sources/OpenAI/Public/Parameters/Chat/ChatCompletionParameters.swift
@@ -429,7 +429,9 @@ public struct ChatCompletionParameters: Encodable {
       stop: [String]? = nil,
       temperature: Double? = nil,
       topProbability: Double? = nil,
-      user: String? = nil)
+      user: String? = nil,
+      streamOptions: StreamOptions
+   )
    {
       self.messages = messages
       self.model = model.value
@@ -455,5 +457,6 @@ public struct ChatCompletionParameters: Encodable {
       self.temperature = temperature
       self.topP = topProbability
       self.user = user
+      self.streamOptions = streamOptions
    }
 }
